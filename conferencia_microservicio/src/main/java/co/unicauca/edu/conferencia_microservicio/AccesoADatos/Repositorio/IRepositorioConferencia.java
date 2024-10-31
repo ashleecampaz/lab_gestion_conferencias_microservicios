acfa269f056011b9bcb51bdb1ba00868ae99e0b7
Ashlee Campaz <ashleecampaz@unicauca.edu.co>
2024-10-31 00:17:38 -0500

    quite los microservicio de notify y conferencias

--- conferencia_microservicio/src/main/java/co/unicauca/edu/conferencia_microservicio/AccesoADatos/Repositorio/IRepositorioConferencia.java
+++
@@ -1,8 +0,0 @@
-package co.unicauca.edu.conferencia_microservicio.AccesoADatos.Repositorio;
-import org.hibernate.mapping.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import co.unicauca.edu.conferencia_microservicio.AccesoADatos.modelos.Conferencia;
-
-public interface IRepositorioConferencia extends JpaRepository<Conferencia, Integer> {
-   
-}
